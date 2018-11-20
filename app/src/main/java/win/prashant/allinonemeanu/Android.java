f843119b8aa6e167e2b175415ac4f2fb4f1454c0
[name] <prashant.patchi@gmail.com>
2018-11-20 01:25:53 +0100

    Initial commit

---
+++ app/src/main/java/win/prashant/allinonemeanu/Android.java
@@ -0,0 +1,13 @@
+package win.prashant.allinonemeanu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Android extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android);
+    }
+}
